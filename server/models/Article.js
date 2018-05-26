ad728e7007fa6b91bfe7104f5f83cd776c73f49e
JohnTheo13 <theodosioujohn88@gmail.com>
2018-05-26 14:27:39 +0200

    server setup

---
+++ server/models/Article.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+
+let ArticleSchema = new mongoose.Schema(
+  {
+    text: String,
+    title: String,
+    description: String,
+    claps: Number,
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User'
+    },
+    comments: [
+      {
+        author: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: 'User'
+        },
+        text: String
+      }
+    ]
+  }
+);
+
+ArticleSchema.methods.clap = () => {
+  this.claps++
+  return this.save();
+}
+
+ArticleSchema.methods.comment = comment => {
+  this.comments.push(comment);
+  return this.save();
+}
+
+ArticleSchema.methods.addAuthor = author_id => {
+  this.author = author_id;
+  return this.save();
+}
+
+ArticleSchema.methods.getUserArticle = _id => {
+  Article.find({ 'author': _id }).then(article => article)
+}
+
+module.exports = mongoose.model('Article', ArticleSchema);
---
+++ server/routes/article.js
@@ -0,0 +1,26 @@
+const articlecontroller = require('./../controllers/article.ctrl')
+const multipart = require('connect-multiparty')
+const multipartWare = multipart()
+
+module.exports = router => {
+  /**
+  * get all articles
+  **/
+  router.route('/articles')
+    .get(articlecontroller.getAll)
+    /**
+    * add an article
+    **/
+  router.route('/article')
+    .post(multipartWare, articlecontroller.addArticle)
+    /**
+    * commet on articles
+    **/
+  router.route('/article/comment')
+    .post(articlecontroller.commentArticle)
+    /**
+    * get specific article to view
+    **/
+  router.route('/article/:id')
+    .get(articlecontroller.getArticle)
+}
